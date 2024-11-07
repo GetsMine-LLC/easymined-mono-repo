9aaefbec03690c7b61628db7982142dba445d70f
Qoupha <109043527+Qoupha@users.noreply.github.com>
2024-11-07 15:27:15 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'easymined-knowledge',
+            script: 'pnpm run start:knowledge-base',
+            cwd: '/home/easymined-mono-repo',
+        },
+    ],
+};
